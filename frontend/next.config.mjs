345a471440cfd285a0b69a9bf1cfefe20ab3fb01
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-09-24 16:10:07 -0400

    edited config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Ensure the export option is set for static export
+  output: "export",
+  images: {
+    unoptimized: true, // Disables image optimization for static export
+  },
 };
 
 export default nextConfig;
