ef785875cf0152b9afffe43bfb94a366f666ae90
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-09-24 16:17:32 -0400

    updated utils

--- frontend/src/utils/fetchProduct1.ts
+++ frontend/src/utils/fetchProduct1.ts
@@ -1,4 +1,3 @@
-// utils/fetchProduct1.ts
 export interface Products1 {
   imageurl: string;
   id: number;
@@ -9,8 +8,13 @@ export interface Products1 {
   type: string;
 }
 
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://shopvoyage.onrender.com"
+    : "http://localhost:5001"; // Switch based on environment
+
 export const fetchProducts1 = async (): Promise<Products1[]> => {
-  const response = await fetch("http://localhost:5001/api/products1");
+  const response = await fetch(`${BASE_URL}/api/products1`);
   if (!response.ok) {
     throw new Error("Failed to fetch products1");
   }
@@ -18,12 +22,41 @@ export const fetchProducts1 = async (): Promise<Products1[]> => {
 };
 
 export const fetchProductById = async (id: number): Promise<Products1> => {
-  const response = await fetch(`http://localhost:5001/api/products1/${id}`);
+  const response = await fetch(`${BASE_URL}/api/products1/${id}`);
   if (!response.ok) {
     throw new Error("Failed to fetch product");
   }
   return response.json();
 };
+
+// // utils/fetchProduct1.ts
+// export interface Products1 {
+//   imageurl: string;
+//   id: number;
+//   name: string;
+//   price: number;
+//   imageUrl: string;
+//   category: string;
+//   type: string;
+// }
+
+// export const fetchProducts1 = async (): Promise<Products1[]> => {
+//   const response = await fetch("http://localhost:5001/api/products1");
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch products1");
+//   }
+//   return response.json();
+// };
+
+// export const fetchProductById = async (id: number): Promise<Products1> => {
+//   const response = await fetch(`http://localhost:5001/api/products1/${id}`);
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch product");
+//   }
+//   return response.json();
+// };
+
+///////////////////////////////////////////
 
 // export interface Products1 {
 //   imageurl: string;
