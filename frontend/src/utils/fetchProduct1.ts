c55a42307f13377c8d27366abdae13a0c5505fa3
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-09-24 20:20:08 -0400

    edited utils

--- frontend/src/utils/fetchProduct1.ts
+++ frontend/src/utils/fetchProduct1.ts
@@ -8,15 +8,13 @@ export interface Products1 {
   type: string;
 }
 
-// const BASE_URL =
-//   process.env.NODE_ENV === "production"
-//     ? "https://shopvoyage.onrender.com"
-//     : "http://localhost:5001"; // Switch based on environment
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://shopvoyage-backend.onrender.com"
+    : "http://localhost:5001"; // Switch based on environment
 
 export const fetchProducts1 = async (): Promise<Products1[]> => {
-  const response = await fetch(
-    `https://shopvoyage-backend.onrender.com/api/products1`
-  );
+  const response = await fetch(`${BASE_URL}/api/products1`);
   if (!response.ok) {
     throw new Error("Failed to fetch products1");
   }
@@ -24,14 +22,37 @@ export const fetchProducts1 = async (): Promise<Products1[]> => {
 };
 
 export const fetchProductById = async (id: number): Promise<Products1> => {
-  const response = await fetch(
-    `https://shopvoyage-backend.onrender.com/api/products1/${id}`
-  );
+  const response = await fetch(`${BASE_URL}/api/products1/${id}`);
   if (!response.ok) {
     throw new Error("Failed to fetch product");
   }
   return response.json();
 };
+
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://shopvoyage.onrender.com"
+//     : "http://localhost:5001"; // Switch based on environment
+
+// export const fetchProducts1 = async (): Promise<Products1[]> => {
+//   const response = await fetch(
+//     `https://shopvoyage-backend.onrender.com/api/products1`
+//   );
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch products1");
+//   }
+//   return response.json();
+// };
+
+// export const fetchProductById = async (id: number): Promise<Products1> => {
+//   const response = await fetch(
+//     `https://shopvoyage-backend.onrender.com/api/products1/${id}`
+//   );
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch product");
+//   }
+//   return response.json();
+// };
 
 // // utils/fetchProduct1.ts
 // export interface Products1 {
