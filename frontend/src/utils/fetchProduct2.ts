7f8e44ab399f561e7002cbce779a0d5775165a7a
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-09-28 15:15:27 -0400

    changed utils

--- frontend/src/utils/fetchProduct2.ts
+++ frontend/src/utils/fetchProduct2.ts
@@ -1,15 +1,15 @@
-export interface Products2 {
-  imageurl: string;
-  id: number;
-  name: string;
-  price: number;
-  imageUrl: string;
-}
+// export interface Products2 {
+//   imageurl: string;
+//   id: number;
+//   name: string;
+//   price: number;
+//   imageUrl: string;
+// }
 
-export const fetchProducts2 = async (): Promise<Products2[]> => {
-  const response = await fetch("http://localhost:5001/api/products2");
-  if (!response.ok) {
-    throw new Error("Failed to fetch products2");
-  }
-  return response.json();
-};
+// export const fetchProducts2 = async (): Promise<Products2[]> => {
+//   const response = await fetch("http://localhost:5001/api/products2");
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch products2");
+//   }
+//   return response.json();
+// };
