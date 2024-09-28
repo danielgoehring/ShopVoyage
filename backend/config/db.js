6c67c341a742d92ebe4d7d5151fa2c11eae60a3b
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-09-28 14:10:16 -0400

    updated db.js, server and utils

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,25 +1,25 @@
 // // import { Pool } from "pg";
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   user: "shopvoyageuser",
-//   host: "localhost",
-//   database: "ShopVoyagedatabase",
-//   password: "password",
-//   port: 5432, // Default PostgreSQL port
-// });
-
-// module.exports = pool;
-
-// db.js or wherever you set up the PostgreSQL connection
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER, // Database username
-  host: process.env.DB_HOST, // Database host
-  database: process.env.DB_NAME, // Database name
-  password: process.env.DB_PASSWORD, // Database password
-  port: process.env.DB_PORT || 5432, // Database port
+  user: "shopvoyageuser",
+  host: "localhost",
+  database: "ShopVoyagedatabase",
+  password: "password",
+  port: 5432, // Default PostgreSQL port
 });
 
 module.exports = pool;
+
+// db.js or wherever you set up the PostgreSQL connection
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: process.env.DB_USER, // Database username
+//   host: process.env.DB_HOST, // Database host
+//   database: process.env.DB_NAME, // Database name
+//   password: process.env.DB_PASSWORD, // Database password
+//   port: process.env.DB_PORT || 5432, // Database port
+// });
+
+// module.exports = pool;
